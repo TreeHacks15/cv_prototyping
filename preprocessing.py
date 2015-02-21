8c2564a7205dc9019df759f3271d980b979d79ca
veered <lucashansen@gmail.com>
2015-02-21 15:18:49 -0800

    Created preprocessing class

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+from scipy.misc import imresize
+import cv2
+
+class Preprocess_T(object):
+	""" 
+		downsample image and convert to grayscale 
+	"""
+
+	def __init__(self, scale=.1):
+		self.scale = scale
+
+	def fit(self, data):
+		return self
+		
+	def transform(self, data):
+	    img = imresize(img, self.scale)
+	    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	    return img
