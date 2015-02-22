c0186b16634bdcc68266c42045f39b9db687656c
Jay Hack <jhack@stanford.edu>
2015-02-21 16:05:41 -0800

    renamed pytesser

---
+++ ocr_transform.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy as sp
+
+class OCR_T(object):
+	"""
+		Transform: OCR
+		--------------
+		image -> contained text
+
+		uses tesseract
+	"""
+	def __init__(self):
+		pass
+
+
+	def fit(self, data):
+		return self
+
+
+	def transform(self, data):
+		"""
+			image -> contained text 
+		"""
+		image = data
+		
